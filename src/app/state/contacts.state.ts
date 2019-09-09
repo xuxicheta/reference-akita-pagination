454ea28297e10b0af37c00781dc738bd5ed0425a
Anton Shvets <antshvets@gmail.com>
2019-09-09 18:00:40 +0300

    minor

--- src/app/state/contacts.state.ts
+++ src/app/state/contacts.state.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ActiveState, createEntityQuery, createEntityStore, EntityState, ID, PaginationResponse, PaginatorPlugin } from '@datorama/akita';
+import { createEntityQuery, createEntityStore, EntityState, ID, PaginationResponse, PaginatorPlugin } from '@datorama/akita';
 import { Observable } from 'rxjs/internal/Observable';
 import { pluck, switchMap } from 'rxjs/operators';
 
@@ -9,36 +9,23 @@ export interface Contact {
   name: string;
 }
 
-function createInitialState() {
-  return {
-    activePage: null,
-  };
-}
-
-interface ContactEntityState extends EntityState<Contact>, ActiveState { }
+interface ContactEntityState extends EntityState<Contact, ID> { }
 
 @Injectable({ providedIn: 'root' })
 export class ContactsState {
-  private store = createEntityStore<ContactEntityState>(createInitialState(), { name: 'contacts', idKey: 'contactId' });
+  private store = createEntityStore<ContactEntityState>({}, { name: 'contacts', idKey: 'contactId' });
   private query = createEntityQuery<ContactEntityState>(this.store, {});
   private paginator = new PaginatorPlugin(this.query).withControls().withRange();
 
   public all$: Observable<Contact[]> = this.query.selectAll();
   public count$: Observable<number> = this.query.selectCount();
 
-  public active$ = this.query.selectActive() as Observable<Contact>;
-  public pageChanges: Observable<number> = this.paginator.pageChanges;
-
-  get active(): Contact {
-    return this.query.getActive() as Contact;
-  }
-
   constructor(
     private http: HttpClient,
   ) { }
 
   private get(page: number): Observable<PaginationResponse<Contact>> {
-    console.log(`requested page number ${page}`);
+    console.log(`requesting page number ${page}`);
     const params = new HttpParams().set('page', `${page}`);
     return this.http.get<PaginationResponse<Contact>>('contacts', { params });
   }
@@ -48,7 +35,7 @@ export class ContactsState {
   }
 
   public selectPage(): Observable<PaginationResponse<Contact>> {
-    return this.pageChanges.pipe(
+    return this.paginator.pageChanges.pipe(
       switchMap(page => this.paginator.getPage(() => this.get(page)))
     );
   }
